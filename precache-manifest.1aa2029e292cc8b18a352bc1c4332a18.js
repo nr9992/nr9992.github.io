3c34896b904c709f426dba6688e43419f80c48b2
System Administrator <root@Neel.local>
2019-03-09 23:28:13 +0530

    Updates

---
+++ precache-manifest.1aa2029e292cc8b18a352bc1c4332a18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "50df2cdb952d12e0d6ca",
+    "url": "/static/js/main.c0dc395f.chunk.js"
+  },
+  {
+    "revision": "a539ea9d3b1acab9c730",
+    "url": "/static/js/2.948d4d77.chunk.js"
+  },
+  {
+    "revision": "50df2cdb952d12e0d6ca",
+    "url": "/static/css/main.5bfd883f.chunk.css"
+  },
+  {
+    "revision": "5a80e47b0f86d62a937ab8c1627f0e02",
+    "url": "/index.html"
+  }
+];
